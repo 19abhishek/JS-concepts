d84402c639b61b92e67cb259c1cc36eb7fa6176f
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-03-11 14:34:42 +0530

    Sum

--- sum/script.js
+++ sum/script.js
@@ -11,3 +11,13 @@ let sum2 = (a) => (b) => b ? sum2(a + b) : a;
 
 console.log(sum(2)(3)());
 console.log(sum2(24)(3)(3)());
+
+(function () {
+  var array1 = [];
+  var array2 = new Array(100);
+  var array3 = new Array(["1", 2, "3", 4, 5.6]);
+  console.log(array1);
+  console.log(array2);
+  console.log(array3);
+  console.log(array3.length);
+})();
