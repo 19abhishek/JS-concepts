63837b674976b020d9cd753c0f18e25b8fe1cc4a
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-03-14 00:40:08 +0530

    Star Rating completed

--- star-rating/script.js
+++ star-rating/script.js
@@ -3,10 +3,37 @@ function Star(el, count, callback) {
   for (let i = 0; i < count; i++) {
     var currStar = document.createElement("i");
     currStar.classList.add("fa");
-    currStar.classList.add("fa-star");
+    currStar.classList.add("fa-star-o");
     starEl.appendChild(currStar);
   }
-  console.log(starEl);
+  const allStars = document.querySelectorAll(".fa");
+  document.querySelectorAll(".fa").forEach((currStar, index) => {
+    currStar.addEventListener("mouseover", () => {
+      for (let i = 0; i <= index; i++) {
+        const cStar = allStars[i];
+        cStar.classList.remove("fa-star-o");
+        cStar.classList.add("fa-star");
+      }
+    });
+
+    function leaveEvent() {
+      for (let i = 0; i <= index; i++) {
+        const cStar = allStars[i];
+        cStar.classList.remove("fa-star");
+        cStar.classList.add("fa-star-o");
+      }
+    }
+    currStar.addEventListener("mouseleave", leaveEvent);
+    currStar.addEventListener("click", () => {
+      for (let i = 0; i <= index; i++) {
+        const cStar = allStars[i];
+        cStar.removeEventListener("mouseleave", leaveEvent);
+        cStar.classList.remove("fa-star-o");
+        cStar.classList.add("fa-star");
+        getStar(i + 1);
+      }
+    });
+  });
 }
 
 function getStar(value) {
