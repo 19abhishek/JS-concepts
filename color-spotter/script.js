74ad521f5f38aa9af554f192a0f79267c547edee
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-03-18 15:52:34 +0530

    Color spotter

---
+++ color-spotter/script.js
@@ -0,0 +1,26 @@
+alert("hi");
+const getRandomColors = function () {
+  var ratio = 0.618033988749895;
+
+  var hue = (Math.random() + ratio) % 1;
+  var saturation = Math.round(Math.random() * 100) % 85;
+  var lightness = Math.round(Math.random() * 100) % 85;
+
+  var color =
+    "hsl(" + Math.round(360 * hue) + "," + saturation + "%," + lightness + "%)";
+  var oddColor =
+    "hsl(" +
+    Math.round(360 * hue) +
+    "," +
+    saturation +
+    "%," +
+    (lightness + 5) +
+    "%)";
+
+  return {
+    color,
+    oddColor,
+  };
+};
+
+console.log(getRandomColors());
