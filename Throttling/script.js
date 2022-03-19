652dbd285bdf4cacc288601cd9c7c6d48fd424c1
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-03-19 18:48:02 +0530

    Throttling

--- Throttling/script.js
+++ Throttling/script.js
@@ -4,18 +4,18 @@ function increaseCount(fname, lname) {
   console.log(count, fname, lname);
 }
 
-const throttle = (func, time, ...args) => {
-  let flag = true;
-  return function () {
-    if (flag) {
-      func.call(this, ...args);
-      flag = false;
-      setTimeout(() => {
-        flag = true;
-      }, time);
-    }
-  };
-};
+// const throttle = (func, time, ...args) => {
+//   let flag = true;
+//   return function () {
+//     if (flag) {
+//       func.call(this, ...args);
+//       flag = false;
+//       setTimeout(() => {
+//         flag = true;
+//       }, time);
+//     }
+//   };
+// };
 
 const fname = "Abhishek";
 const lname = "Kumar";
